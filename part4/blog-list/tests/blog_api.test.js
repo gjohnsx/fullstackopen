1eaa520e11e937d2706fe74c0e29b200100c4479
gjohnsx <gregjohns@fastmail.com>
2022-06-21 16:36:35 -0400

    ex 4.13 working

--- part4/blog-list/tests/blog_api.test.js
+++ part4/blog-list/tests/blog_api.test.js
@@ -19,77 +19,97 @@ beforeEach(async () => {
 
 
 // * Tests:
-test('blogs are returned as json', async () => {
-  await api
-    .get('/api/blogs')
-    .expect(200)
-    .expect('Content-Type', /application\/json/);
+describe('when there are initially some blogs saved', () => {
+  test('blogs are returned as json', async () => {
+    await api
+      .get('/api/blogs')
+      .expect(200)
+      .expect('Content-Type', /application\/json/);
+  });
+
+  test('all blogs are returned', async () => {
+    const response = await api.get('/api/blogs');
+
+    expect(response.body).toHaveLength(helper.initialBlogs.length);
+  });
+
+  test('unique identifier property of blog post is named id', async () => {
+    const blogs = await api.get('/api/blogs');
+    expect(blogs.body[0].id).toBeDefined();
+  });
 });
 
-test('all blogs are returned', async () => {
-  const response = await api.get('/api/blogs');
+describe('posting a new blog', () => {
+  test('succeeds with status code 201 with valid data', async () => {
+    const newBlog = {
+      title: 'New blog API test',
+      author: 'gjohnsx',
+      url: 'http://gjohns.xyz',
+      likes: 0,
+    };
 
-  expect(response.body).toHaveLength(helper.initialBlogs.length);
+    await api
+      .post('/api/blogs')
+      .send(newBlog)
+      .expect(201)
+      .expect('Content-Type', /application\/json/);
+
+    const blogsAtEnd = await helper.blogsInDb();
+    expect(blogsAtEnd).toHaveLength(helper.initialBlogs.length + 1);
+
+    const titles = blogsAtEnd.map(blog => blog.title);
+    expect(titles).toContainEqual('New blog API test');
+  });
+
+  test('likes default to 0 if undefined in new blog creation', async () => {
+    const newBlogWithoutLikes = {
+      title: 'New blog without likes',
+      author: 'gjohnsx',
+      url: 'http://gjohns.xyz/no-likes',
+    };
+
+    await api
+      .post('/api/blogs')
+      .send(newBlogWithoutLikes)
+      .expect(201)
+      .expect('Content-Type', /application\/json/);
+
+    const blogsAtEnd = await helper.blogsInDb();
+
+    const blogToView = blogsAtEnd[blogsAtEnd.length -1];
+    expect(blogToView.likes).toEqual(0);
+  });
+
+  test('fails with status code 400 if no title or no url supplied', async () => {
+    const newBlogWithoutTitleUrl = {
+      author: 'gjohnsx',
+    };
+
+    await api
+      .post('/api/blogs')
+      .send(newBlogWithoutTitleUrl)
+      .expect(400);
+
+    const blogsAtEnd = await helper.blogsInDb();
+    expect(blogsAtEnd).toHaveLength(helper.initialBlogs.length);
+  });
 });
 
-test('unique identifier property of blog post is named id', async () => {
-  const blogs = await api.get('/api/blogs');
-  expect(blogs.body[0].id).toBeDefined();
-});
+describe('deleting a blog post', () => {
+  test.only('succeeds with status code 204 with valid id', async () => {
+    const blogsAtStart = await helper.blogsInDb();
+    const blogToDelete = blogsAtStart[0];
 
-// Posting a blog:
-test('posting a new blog works', async () => {
-  const newBlog = {
-    title: 'New blog API test',
-    author: 'gjohnsx',
-    url: 'http://gjohns.xyz',
-    likes: 0,
-  };
+    await api
+      .delete(`/api/blogs/${blogToDelete.id}`)
+      .expect(204);
 
-  await api
-    .post('/api/blogs')
-    .send(newBlog)
-    .expect(201)
-    .expect('Content-Type', /application\/json/);
+    const blogsAtEnd = await helper.blogsInDb();
 
-  const blogsAtEnd = await helper.blogsInDb();
-  expect(blogsAtEnd).toHaveLength(helper.initialBlogs.length + 1);
-
-  const titles = blogsAtEnd.map(blog => blog.title);
-  expect(titles).toContainEqual('New blog API test');
-});
-
-test('new blog likes defaults to 0 if undefined', async () => {
-  const newBlogWithoutLikes = {
-    title: 'New blog without likes',
-    author: 'gjohnsx',
-    url: 'http://gjohns.xyz/no-likes',
-  };
-
-  await api
-    .post('/api/blogs')
-    .send(newBlogWithoutLikes)
-    .expect(201)
-    .expect('Content-Type', /application\/json/);
-
-  const blogsAtEnd = await helper.blogsInDb();
-
-  const blogToView = blogsAtEnd[blogsAtEnd.length -1];
-  expect(blogToView.likes).toEqual(0);
-});
-
-test('new blog without title and url is not created', async () => {
-  const newBlogWithoutTitleUrl = {
-    author: 'gjohnsx',
-  };
-
-  await api
-    .post('/api/blogs')
-    .send(newBlogWithoutTitleUrl)
-    .expect(400);
-
-  const blogsAtEnd = await helper.blogsInDb();
-  expect(blogsAtEnd).toHaveLength(helper.initialBlogs.length);
+    expect(blogsAtEnd).toHaveLength(helper.initialBlogs.length - 1);
+    const titles = blogsAtEnd.map(blog => blog.title);
+    expect(titles).not.toContain(blogToDelete.title);
+  });
 });
 
 describe('Likes', () => {
