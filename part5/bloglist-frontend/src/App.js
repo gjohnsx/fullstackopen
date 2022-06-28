6c08e221bc42a335dbe37bc5c204e0b0f57e1470
gjohnsx <gregjohns@fastmail.com>
2022-06-28 10:53:24 -0400

    ex 5.1 working

--- part5/bloglist-frontend/src/App.js
+++ part5/bloglist-frontend/src/App.js
@@ -1,9 +1,13 @@
-import { useState, useEffect } from 'react'
-import Blog from './components/Blog'
-import blogService from './services/blogs'
+import { useState, useEffect } from 'react';
+import Blog from './components/Blog';
+import blogService from './services/blogs';
+import loginService from './services/login';
 
 const App = () => {
-  const [blogs, setBlogs] = useState([])
+  const [blogs, setBlogs] = useState([]);
+  const [user, setUser] = useState(null);
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
 
   useEffect(() => {
     blogService.getAll().then(blogs =>
@@ -11,12 +15,62 @@ const App = () => {
     )  
   }, [])
 
+  const handleLogin = async (event) => {
+    event.preventDefault();
+    console.log('Logging in', username)
+
+    try {
+      const user = await loginService.login({
+        username, password
+      })
+
+      console.log('did we get the user back?\n', user);
+
+      blogService.setToken(user.token);
+      setUser(user);
+      setUsername('');
+      setPassword('');
+    } catch (error) {
+      console.log(error);
+      alert(error.message);
+    }
+  };
+
+  const blogsDisplay = () => (
+    blogs.map(blog => 
+      <Blog key={blog.id} blog={blog} />
+      )
+  )
+
+  if (user === null) {
+    return (
+        <div>
+          <h1>log in to application</h1>
+          <form onSubmit={handleLogin}>
+            <input 
+              type='text' 
+              placeholder='username' 
+              name='username'
+              value={username}
+              onChange={({ target }) => setUsername(target.value)}
+            />
+            <input
+              type='password'
+              placeholder='password'
+              name='password'
+              value={password}
+              onChange={({ target }) => setPassword(target.value)}
+            />
+            <button>log in</button>
+          </form>
+        </div>
+    )
+  }
+
   return (
     <div>
       <h2>blogs</h2>
-      {blogs.map(blog =>
-        <Blog key={blog.id} blog={blog} />
-      )}
+      {blogsDisplay()}
     </div>
   )
 }
