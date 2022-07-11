e25f3a340d66d365ff8937f8a69ef3234051f046
gjohnsx <gregjohns@fastmail.com>
2022-07-11 15:29:39 -0400

    added json-server initialization

--- part6/redux-anecdotes/src/index.js
+++ part6/redux-anecdotes/src/index.js
@@ -4,8 +4,14 @@ import { store } from './store';
 import { Provider } from 'react-redux';
 import App from './App';
 import './index.css';
+import anecdoteService from './services/anecdotes';
+import { setAnecdotes } from './reducers/anecdoteReducer';
 
-console.log('getState:', store.getState());
+anecdoteService.getAll().then(anecdotes => 
+  store.dispatch(setAnecdotes(anecdotes))
+);
+
+// console.log('getState:', store.getState());
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <Provider store={store}>
--- part6/redux-anecdotes/src/reducers/anecdoteReducer.js
+++ part6/redux-anecdotes/src/reducers/anecdoteReducer.js
@@ -1,24 +1,10 @@
 import { createSlice } from '@reduxjs/toolkit';
 
-const initialState = [
-  { content: 'If it hurts, do it more often', votes: 0, id: 1 },
-  { content: 'Adding manpower to a late software project makes it later!', votes: 0, id: 2 },
-];
-
-// const initialState = [
-//   { content: 'If it hurts, do it more often', votes: 0, id: 1 },
-//   { content: 'Adding manpower to a late software project makes it later!', votes: 0, id: 2 },
-//   { content: 'The first 90 percent of the code accounts for the first 90 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.', votes: 0, id: 3 },
-//   { content: 'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.', votes: 0, id: 4 },
-//   { content: 'Premature optimization is the root of all evil.', votes: 0, id: 5 },
-//   { content: 'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.', votes: 0, id: 6 },
-// ];
-
 const getId = () => (1000000 * Math.random()).toFixed(0)
 
 const anecdoteSlice = createSlice({
   name: 'anecdotes',
-  initialState,
+  initialState: [],
   reducers: {
     createAnecdote(state, action) {
       const content = action.payload;
@@ -39,8 +25,11 @@ const anecdoteSlice = createSlice({
       const anecdoteToDownvote = state.find(a => a.id === id);
       anecdoteToDownvote.votes --;
     },
+    setAnecdotes(state, action) {
+      return action.payload;
+    }
   }
 });
 
-export const { createAnecdote, upvote, downvote } = anecdoteSlice.actions;
+export const { createAnecdote, upvote, downvote, setAnecdotes } = anecdoteSlice.actions;
 export default anecdoteSlice.reducer;
